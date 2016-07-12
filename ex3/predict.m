0fae7c36939a96daa4f3d17475d3a8eb48135bc7
Jemin Lee <leejaymin@gmail.com>
2016-07-12 05:59:39 -0700

    solved ex3 add .gitignore file make directory strucuture for python

--- ex3/predict.m
+++ ex3/predict.m
@@ -7,12 +7,12 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
+%               your learned neural network. You should set p to a
 %               vector containing labels between 1 to num_labels.
 %
 % Hint: The max function might come in useful. In particular, the max
@@ -21,13 +21,18 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+a1 = [ones(m, 1) X]; % add 1
 
+z2 = a1 * Theta1';
 
+a2 = [ones(size(sigmoid(z2), 1), 1) sigmoid(z2)]; % add 1
 
+z3 = a2 * Theta2';
+a3 = sigmoid(z3); % H_theta(x)
 
+[x, ix] = max(a3, [], 2);
 
-
-
+p = ix;
 
 % =========================================================================
 
